41915dce3180830c2fc6af3a712ee2d69c0dc04a
aidenmartin864 <112721864+aidenmartin864@users.noreply.github.com>
2022-10-15 12:20:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const fonts = ['sans-serif','monospace','cursive','arial','verdana','tahoma','times new roman'];
+const fonts = ['sans-serif','monospace','cursive','Arial','Verdana','Tahoma','times new roman','brush script mt', 'georgia','garamond'];
 
-document.getElementById("button").addEventListener("click", function(event) {
+document.getElementById("kanye").addEventListener("click", function(event) {
     event.preventDefault();
     const url = 'https://api.kanye.rest';
     fetch(url)
         .then(function(response){
             return response.json();
         }).then(function(json){
-            let rand = parseInt((Math.random()*7));
+            let rand = parseInt((Math.random()*10));
             let newQuote = json.quote;
             document.getElementById('quote').textContent = newQuote;
+            document.getElementById('quote').style.fontWeight = 'normal';
             document.getElementById('quote').style.fontFamily = fonts[rand];
         });
 });
